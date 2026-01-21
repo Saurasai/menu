480ffd79ea8780b5747a7f5c273491f33d18e66e
saurab <96380472+Saurasai@users.noreply.github.com>
2026-01-21 19:59:35 +0530

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('hello world ')
+    return HttpResponse('hello world')
 
-class helloethiopia(View):
-    def get(self,request):
-        return HttpResponse('hello ethiopia')
+class hfc(View):
+    def get(self, request):
+        return HttpResponse('hello from class')
